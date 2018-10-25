70f0feffbd620bbd207e2a1636cde20cd54118c0
llxunuo <llxunuo@163.com>
2018-10-25 19:13:58 +0800

    add

---
+++ src/test/java/zipkinDemo/FirstServiceTest.java
@@ -0,0 +1,55 @@
+package zipkinDemo; 
+
+import org.junit.Test; 
+import org.junit.Before; 
+import org.junit.After; 
+
+/** 
+* FirstService Tester. 
+* 
+* @author <Authors name> 
+* @since <pre>ÆßÔÂ 17, 2018</pre> 
+* @version 1.0 
+*/ 
+public class FirstServiceTest { 
+
+@Before
+public void before() throws Exception { 
+} 
+
+@After
+public void after() throws Exception { 
+} 
+
+/** 
+* 
+* Method: restTemplate() 
+* 
+*/ 
+@Test
+public void testRestTemplate() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: callOther() 
+* 
+*/ 
+@Test
+public void testCallOther() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+/** 
+* 
+* Method: main(String[] args) 
+* 
+*/ 
+@Test
+public void testMain() throws Exception { 
+//TODO: Test goes here... 
+} 
+
+
+} 
